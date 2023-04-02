4e9fb3fa85d9800e0fceae53219c77bab81be375
Serhii Demchenko <svdemchenko@gmail.com>
2023-04-02 22:15:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "./firebase";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <App />
       </Provider>
